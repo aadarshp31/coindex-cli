55cb137df603bf679e8fd9dca7bde6ce1483e6e1
adarsh.pandey <adarsh.pandey@zohocorp.com>
2021-10-01 00:19:18 +0530

    implemented sub cmds for parent cmd 'key'

--- bin/commands/key.js
+++ bin/commands/key.js
@@ -1,12 +1,57 @@
+const KeyManager = require('../lib/KeyManager');
+const inquirer = require('inquirer');
+const colors = require('ansi-colors');
+const { isRequired } = require('../utils/validation');
+
 const key = {
-	set() {
-		console.log('Hello from set');
+	async set() {
+		const keyManager = new KeyManager();
+		const input = await inquirer.prompt([
+			{
+				type: 'input',
+				name: 'apiKey',
+				message: colors.bold('Enter API key ') + 'https://nomics.com',
+				validate: isRequired,
+			},
+		]);
+
+		const apiKey = keyManager.setKey(input.apiKey);
+
+		if (apiKey) {
+			console.log(colors.bold.blue('API key set!'));
+		}
 	},
-	show() {
-		console.log('Hello from show');
+	async show() {
+		try {
+			const keyManager = new KeyManager();
+			const apiKey = keyManager.getKey();
+			console.log(colors.bold('Current API Key: ') + colors.bold.cyan(apiKey));
+		} catch (error) {
+			console.log(colors.bold.red(error.message));
+			const input = await inquirer.prompt([
+				{
+					type: 'confirm',
+					name: 'consent',
+					message: colors.bold.blue('Do you want to set a new API Key now?'),
+					default: true,
+				},
+			]);
+
+			if (!input.consent) {
+				return;
+			}
+			this.set().then(() => this.show());
+			return;
+		}
 	},
 	remove() {
-		console.log('Hello from remove');
+		const keyManager = new KeyManager();
+		try {
+			keyManager.deleteKey();
+			console.log(colors.bold('API Key deleted successfully!'));
+		} catch (error) {
+			console.log(colors.bold.red(error.message));
+		}
 	},
 };
 
